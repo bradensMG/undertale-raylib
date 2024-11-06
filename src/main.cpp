0e7a38313b9f423d9417665b744b8c8d393219ca
braden <130822964+bradensMG@users.noreply.github.com>
2024-11-05 21:19:41 -0500

    Idk

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@ int main()
 {
     constexpr int screenWidth = 640;
     constexpr int screenHeight = 480;
-    
     InitWindow(screenWidth, screenHeight, "raylib undertale");
     InitAudioDevice();
     SetTargetFPS(30);
@@ -35,7 +34,15 @@ int main()
         int w = 575;
         int h = 140;
     };
+
+    struct PlayerStats {
+        int hp = 20.0f;
+        int maxhp = 20.0f;
+        std::string name = "Chara";
+    };
+
     ArenaParams arena;
+    PlayerStats playerStats;
 
     Texture2D buttons[] = {fightButton, actButton, itemButton, mercyButton};
 
@@ -60,7 +67,7 @@ int main()
 
             DrawTexture(referenceImage, 0, 0, {255, 255, 255, 0});
 
-            DrawTextEx(uiFont, "Braden   LV 1", (Vector2){30, 400}, 24, 0, WHITE);
+            DrawTextEx(uiFont, (playerStats.name + "   LV 1").c_str(), (Vector2){30, 400}, 24, 0, WHITE);
 
             for (int i = 0; i < 4; ++i) {
                 DrawTextureRec(buttons[i], 
@@ -73,6 +80,9 @@ int main()
 
             DrawRectangle(arena.x, arena.y, arena.w, arena.h, WHITE);
             DrawRectangle(arena.x + 5, arena.y + 5, arena.w - 10, arena.h - 10, BLACK);
+            DrawRectangle(275.0f, 400.0f, playerStats.maxhp * 1.2, 21.0f, {200, 0, 0, 255});
+            DrawRectangle(275.0f, 400.0f, playerStats.hp * 1.2, 21.0f, YELLOW);
+
             DrawTexture(playerImage, player.x, player.y, {255, 0, 0, 255});
         EndDrawing();
     }
@@ -83,6 +93,7 @@ int main()
     UnloadTexture(mercyButton);
     UnloadTexture(hpName);
     UnloadTexture(playerImage);
+    UnloadTexture(referenceImage);
     UnloadFont(uiFont);
     UnloadSound(moveSound);
     
